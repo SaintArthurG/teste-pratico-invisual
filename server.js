8af235a02ae4c0859c4c9c3c6d2e3c890df80542
SaintArthurG <cazooklol@gmail.com>
2025-09-25 17:18:52 -0300

    add: adicionando cabeçalho

--- server.js
+++ server.js
@@ -3,41 +3,43 @@ const express = require('express')
 const xlsx = require('node-xlsx').default
 const fs = require('fs')
 
-
 const PORTA = 3000;
 
 const app = express();
 
 app.use(json())
 
-app.post('/filmes', (req, res)=>{
-    const filmesData  = req.body;
-    
-    let arrayDeFilmes = filmesData.map(filme => [
+app.post('/filmes', (req, res) => {
+    const filmesData = req.body;
+
+    const cabecalho = ['titulo', 'ano', 'duracao', 'nota', 'sinopse'];
+
+    const arrayDeFilmes = filmesData.map(filme => [
         filme.titulo,
         filme.ano,
         filme.duracao,
         filme.nota,
-        filme.sinopse])
+        filme.sinopse
+    ]);
 
-    var buffer = xlsx.build([{
-        name: 'filmesxl', data: arrayDeFilmes
+    const planilha = [cabecalho, ...arrayDeFilmes];
+
+    const buffer = xlsx.build([{
+        name: 'filmesxl', data: planilha
     }]);
 
-    console.log(buffer);
-
-    try{
+    try {
         fs.writeFileSync('filmes.xlsx', buffer)
         res.status(200).json({
-            messagem: "Planilha criada com sucesso!"
+            mensagem: "Planilha criada com sucesso!"
         })
-    } catch(e) {
+    } catch (e) {
         res.status(400).json({
-            error: "Falha ao criar a planilha " + e
+            error: "Falha ao criar a planilha: " + e
         })
     }
 })
 
-app.listen(PORTA, ()=>{
+app.listen(PORTA, () => {
     console.log(`Servidor rodando na porta ${PORTA}`);
 })
